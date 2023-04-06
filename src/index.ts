cdbc3d680db88690d07ff432d23fc2cc482968dd
Valentin Palkovic <valentin@chromatic.com>
2023-04-06 11:04:14 +0200

    fix: Changes on source files were not considered after init
    After the plugin is initialized ones, changes on the source files are not considered anymore.
    
    I have changed the logic, where a new ts program is created for every file change. The program also only considers the actual file and not a glob of all possible files.

--- src/index.ts
+++ src/index.ts
@@ -1,85 +1,82 @@
 import { type Plugin, createFilter } from "vite";
-import * as path from "path";
-import glob from "glob-promise";
 import type { Options } from "./utils/options";
+import type { CompilerOptions } from "typescript";
+
+const getProgram = async (
+  sourcePath: string,
+  compilerOptions: CompilerOptions
+) => {
+  const { default: ts } = await import("typescript");
+
+  return ts.createProgram([sourcePath], compilerOptions);
+};
 
 const getUtils = async (config: Options) => {
-	const docGen = await import("react-docgen-typescript");
-	const { default: ts } = await import("typescript");
-	const { generateDocgenCodeBlock } = await import("./utils/generate");
-	const { getOptions } = await import("./utils/options");
+  const docGen = await import("react-docgen-typescript");
+  const { generateDocgenCodeBlock } = await import("./utils/generate");
+  const { getOptions } = await import("./utils/options");
 
-	const { docgenOptions, compilerOptions, generateOptions } =
-		getOptions(config);
+  const { docgenOptions, compilerOptions, generateOptions } =
+    getOptions(config);
 
-	const docGenParser = docGen.withCompilerOptions(
-		compilerOptions,
-		docgenOptions,
-	);
-	const { exclude = ["**/**.stories.tsx"], include = ["**/**.tsx"] } =
-		docgenOptions;
-	const filter = createFilter(include, exclude);
+  const docGenParser = docGen.withCompilerOptions(
+    compilerOptions,
+    docgenOptions
+  );
+  const { exclude = ["**/**.stories.tsx"], include = ["**/**.tsx"] } =
+    docgenOptions;
+  const filter = createFilter(include, exclude);
 
-	const files = include
-		.map((filePath) =>
-			glob.sync(
-				path.isAbsolute(filePath)
-					? filePath
-					: path.join(process.cwd(), filePath),
-			),
-		)
-		.reduce((carry, files) => carry.concat(files), []);
+  const result = {
+    docGenParser,
+    filter,
+    generateOptions,
+    compilerOptions,
+    generateDocgenCodeBlock,
+  };
 
-	const tsProgram = ts.createProgram(files, compilerOptions);
-
-	const result = {
-		docGenParser,
-		filter,
-		generateOptions,
-		generateDocgenCodeBlock,
-		tsProgram,
-	};
-
-	return result;
+  return result;
 };
 
 export default function reactDocgenTypescript(config: Options = {}): Plugin {
-	const utilsPromise = getUtils(config);
+  const utilsPromise = getUtils(config);
 
-	return {
-		name: "vite:react-docgen-typescript",
-		async transform(src, id) {
-			try {
-				const {
-					filter,
-					docGenParser,
-					generateOptions,
-					generateDocgenCodeBlock,
-					tsProgram,
-				} = await utilsPromise;
+  return {
+    name: "vite:react-docgen-typescript",
+    async transform(src, id) {
+      try {
+        const {
+          filter,
+          docGenParser,
+          generateOptions,
+          compilerOptions,
+          generateDocgenCodeBlock,
+        } = await utilsPromise;
 
-				if (!filter(id)) {
-					return;
-				}
+        const tsProgram = await getProgram(id, compilerOptions);
 
-				const componentDocs = docGenParser.parseWithProgramProvider(
-					id,
-					() => tsProgram,
-				);
+        if (!filter(id)) {
+          return;
+        }
 
-				if (!componentDocs.length) {
-					return null;
-				}
+        const componentDocs = docGenParser.parseWithProgramProvider(
+          id,
+          () => tsProgram
+        );
 
-				return generateDocgenCodeBlock({
-					filename: id,
-					source: src,
-					componentDocs,
-					...generateOptions,
-				});
-			} catch (e) {
-				return src;
-			}
-		},
-	};
+        if (!componentDocs.length) {
+          return null;
+        }
+
+        return generateDocgenCodeBlock({
+          filename: id,
+          source: src,
+          componentDocs,
+          ...generateOptions,
+        });
+      } catch (e) {
+        return src;
+      }
+    },
+  };
 }
